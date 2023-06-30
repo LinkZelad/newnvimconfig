da061622e4d780f60f95f55e75fe4b4739df0725
LinkZelad <guiqi007007@sina.cn>
2023-06-30 20:29:14 +0800

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -355,11 +355,11 @@ You must install Git before installing this Nvim config. See:
 
 	if ($USE_SSH) {
 		if ((check_nvim_version -RequiredVersionMin $REQUIRED_NVIM_VERSION)) {
-			safe_execute -WithCmd { git clone --progress -b "$env:CCLONE_BRANCH" "$env:CCLONE_ATTR" 'git@github.com:ayamir/nvimdots.git' "$env:CCDEST_DIR" }
+			safe_execute -WithCmd { git clone --progress -b "$env:CCLONE_BRANCH" "$env:CCLONE_ATTR" 'git@github.com:linkzelad/newnvimconfig.git' "$env:CCDEST_DIR" }
 		} elseif ((check_nvim_version -RequiredVersionMin $REQUIRED_NVIM_VERSION_LEGACY)) {
 			warn -Msg "You have outdated Nvim installed (< $REQUIRED_NVIM_VERSION)."
 			info -Msg "Automatically redirecting you to the latest compatible version..."
-			safe_execute -WithCmd { git clone --progress -b 0.8 "$env:CCLONE_ATTR" 'git@github.com:ayamir/nvimdots.git' "$env:CCDEST_DIR" }
+			safe_execute -WithCmd { git clone --progress -b 0.8 "$env:CCLONE_ATTR" 'git@github.com:linkzelad/newnvimconfig.git' "$env:CCDEST_DIR" }
 		} else {
 			warn -Msg "You have outdated Nvim installed (< $REQUIRED_NVIM_VERSION_LEGACY)."
 			_abort -Msg "This Neovim distribution is no longer supported." -Type "NotImplemented" -Info_msg @"
@@ -370,11 +370,11 @@ Please make sure you have nvim v$REQUIRED_NVIM_VERSION_LEGACY installed at the v
 		}
 	} else {
 		if ((check_nvim_version -RequiredVersionMin $REQUIRED_NVIM_VERSION)) {
-			safe_execute -WithCmd { git clone --progress -b "$env:CCLONE_BRANCH" "$env:CCLONE_ATTR" 'https://github.com/ayamir/nvimdots.git' "$env:CCDEST_DIR" }
+			safe_execute -WithCmd { git clone --progress -b "$env:CCLONE_BRANCH" "$env:CCLONE_ATTR" 'https://github.com/linkzelad/newnvimconfig.git' "$env:CCDEST_DIR" }
 		} elseif ((check_nvim_version -RequiredVersionMin $REQUIRED_NVIM_VERSION_LEGACY)) {
 			warn -Msg "You have outdated Nvim installed (< $REQUIRED_NVIM_VERSION)."
 			info -Msg "Automatically redirecting you to the latest compatible version..."
-			safe_execute -WithCmd { git clone --progress -b 0.8 "$env:CCLONE_ATTR" 'https://github.com/ayamir/nvimdots.git' "$env:CCDEST_DIR" }
+			safe_execute -WithCmd { git clone --progress -b 0.8 "$env:CCLONE_ATTR" 'https://github.com/linkzelad/newnvimconfig.git' "$env:CCDEST_DIR" }
 		} else {
 			warn -Msg "You have outdated Nvim installed (< $REQUIRED_NVIM_VERSION_LEGACY)."
 			_abort -Msg "This Neovim distribution is no longer supported." -Type "NotImplemented" -Info_msg @"
